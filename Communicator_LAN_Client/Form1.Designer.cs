ff26319f415fa39f760f1a7c93d9073f7978345a
endrjoo96 <3ndrju96@gmail.com>
2018-12-23 10:20:35 +0100

    test

--- Communicator_LAN_Client/Form1.Designer.cs
+++ Communicator_LAN_Client/Form1.Designer.cs
@@ -28,13 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.IP_textBox = new System.Windows.Forms.TextBox();
+            this.Connect_button = new System.Windows.Forms.Button();
+            this.Username_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Password_textBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // IP_textBox
+            // 
+            this.IP_textBox.Location = new System.Drawing.Point(82, 12);
+            this.IP_textBox.Name = "IP_textBox";
+            this.IP_textBox.Size = new System.Drawing.Size(100, 20);
+            this.IP_textBox.TabIndex = 0;
+            // 
+            // Connect_button
+            // 
+            this.Connect_button.Location = new System.Drawing.Point(82, 102);
+            this.Connect_button.Name = "Connect_button";
+            this.Connect_button.Size = new System.Drawing.Size(75, 23);
+            this.Connect_button.TabIndex = 1;
+            this.Connect_button.Text = "button1";
+            this.Connect_button.UseVisualStyleBackColor = true;
+            this.Connect_button.Click += new System.EventHandler(this.Connect_button_Click);
+            // 
+            // Username_textBox
+            // 
+            this.Username_textBox.Location = new System.Drawing.Point(82, 39);
+            this.Username_textBox.Name = "Username_textBox";
+            this.Username_textBox.Size = new System.Drawing.Size(100, 20);
+            this.Username_textBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "nick";
+            // 
+            // Password_textBox
+            // 
+            this.Password_textBox.Location = new System.Drawing.Point(82, 66);
+            this.Password_textBox.Name = "Password_textBox";
+            this.Password_textBox.Size = new System.Drawing.Size(100, 20);
+            this.Password_textBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(364, 386);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Password_textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Username_textBox);
+            this.Controls.Add(this.Connect_button);
+            this.Controls.Add(this.IP_textBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox IP_textBox;
+        private System.Windows.Forms.Button Connect_button;
+        private System.Windows.Forms.TextBox Username_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Password_textBox;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Communicator_LAN_Client/Form1.cs
+++ Communicator_LAN_Client/Form1.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +15,40 @@ namespace Communicator_LAN_Client
 {
     public partial class Form1 : Form
     {
+        TcpListener listener = null;
+        TcpClient client = null;
+        NetworkStream stream = null;
+        BinaryWriter writer = null;
+        BinaryReader reader = null;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Connect_button_Click(object sender, EventArgs e)
+        {
+            Thread x = new Thread(()=>
+            {
+                try
+                {
+                    using (client = new TcpClient(IP_textBox.Text, 65505))
+                    {
+                        using (stream = client.GetStream())
+                        {
+                            string send, received;
+                            send = "LOGIN:"+Username_textBox.Text+"|"+Password_textBox;
+                            writer = new BinaryWriter(stream);
+                            writer.Write(send);
+                            reader = new BinaryReader(stream);
+                        }
+                    }
+                }
+                catch { }
+            });
+            x.IsBackground = true;
+            x.Start();
+            
         }
     }
 }
